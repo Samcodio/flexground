a7a2cca14916a70d70ed1f45b1a37741d1fd23b4
Samcodio <nosikesamuel1@gmail.com>
2024-03-23 12:23:09 -0700

    first commit

--- take_app/views.py
+++ take_app/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render, redirect, reverse
 # Create your views here.
 
 
-def dashboard(request):
+def home(request):
     context = {}
     return render(request, 'take_app/index.html', context)
