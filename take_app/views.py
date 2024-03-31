d578279cc1f0a5082567c60b0bbc0265e2b35bde
Brightnovic <ndububright@gmail.com>
2024-03-31 18:03:41 +0100

    SON FIRST EDIT

--- take_app/views.py
+++ take_app/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, redirect
 
 from .models import Country, Person
 from django.urls import reverse
-import openai , os
+import openai
+import os
 
-api_key = ""
+api_key = "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"
 
 def home(request):
     # print(loader.get_template_dirs())  # Only needed if you're debugging template loading.
@@ -16,26 +17,22 @@ def home(request):
 
 def get_names(request):
     if request.method == 'GET':
+        country = request.GET.get('country')
+        gender = request.GET.get('Gender')
 
-        country = request.POST.get('country')
-        gender = request.POST.get('Gender')
-        if country == "" and gender == "":
-            return redirect(request,'take_app/index.html')
+        if api_key:
+            openai.api_key = api_key
+            response = openai.Completion.create(
+                engine="gpt-3.5-turbo",
+                prompt=f"Generate some {country} names for me, {gender} ones only",
+                max_tokens=50
+            )
+
+            generated_names = response['choices'][0]['text'].strip()
         else:
-            print(gender,country)
-            if api_key:
-                openai.api_key = api_key
-                response = openai.Completion.create(
-                    engine="text-davinci-003",
-                    prompt=f"Generate some {country} names for me, {gender} ones only",
-                    max_tokens=50
-                )
+            generated_names = "Error: OpenAI API key not found"
 
-                generated_names = response.choices[0].text.strip()
-            else:
-                generated_names = "Error: OpenAI API key not found"
-
-            context = {'country': country, 'gender': gender, 'generated_names': generated_names}
+        context = {'country': country, 'gender': gender, 'generated_names': generated_names}
         return render(request, 'take_app/names.html', context)
 
     return render(request, 'take_app/names.html')
