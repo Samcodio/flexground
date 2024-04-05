e4fa1ade83565d379026f3cd6afee414d4e8277f
Sam_El <146867369+Samcodio@users.noreply.github.com>
2024-04-05 12:15:49 -0700

    Update forms.py

--- user_login/forms.py
+++ user_login/forms.py
@@ -1,25 +1,4 @@
-from django.forms import forms
-from django.contrib.auth.forms import UserCreationForm
-from take_app.models import CustomUser
 
-
-class CreateUserForm(UserCreationForm):
-    class Meta:
-        model = CustomUser
-        fields = ['first_name', 'last_name', 'username', 'password1', 'password2']
-
-        def save(self, commit=True):
-            user = super(CreateUserForm, self).save(commit=False)
-            user.first_name = self.cleaned_data['first_name']
-            user.last_name = self.cleaned_data['last_name']
-            user.username = self.cleaned_data['username']
-            user.email = self.cleaned_data['email']
-            user.password1 = self.cleaned_data['password1']
-            user.password2 = self.cleaned_data['password2']
-
-            if commit:
-                user.save()
-                return user
 
 
 
