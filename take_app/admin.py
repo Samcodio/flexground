5c6d79af04170ebaf2bdbe6645a1095787032c43
Sam_El <146867369+Samcodio@users.noreply.github.com>
2024-04-06 08:04:16 -0700

    Update admin.py

--- take_app/admin.py
+++ take_app/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Person, Country, CustomUser
+from .models import Person, Country
 
 
 admin.site.register(Country)
 admin.site.register(Person)
 
 
-admin.site.register(CustomUser)
